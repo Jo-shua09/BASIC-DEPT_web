c00855a16f166dfab7bec2e13bf496a07c4836bc
jo-shua33 <126611915+jo-shua33@users.noreply.github.com>
2024-05-30 15:17:42 -0700

    commit 13

--- js/script.js
+++ js/script.js
@@ -136,34 +136,6 @@ targetBox.addEventListener("mouseout", () => {
 	targetArrow.classList.remove("fa-shake");
 });
 
-// // CLOSE AND OPEN RESPONSIVE NAVBAR
-// const openNav = document.querySelector(".open_nav");
-// const closeNav = document.querySelector(".close_menu");
-// const respNav = document.querySelector(".responsive_navbar");
-
-// openNav.addEventListener("click", () => {
-// 	respNav.classList.add("show");
-// });
-
-// closeNav.addEventListener("click", () => {
-// 	respNav.classList.remove("show");
-// });
-
-// // CLOSE AND OPEN SECTIONS
-// const openInit = document.querySelectorAll("#open_initiative");
-// const closeInit = document.querySelector(".close_initiative");
-// const initiative = document.querySelector(".initiative");
-
-// openInit.forEach((element) => {
-// 	element.addEventListener("click", () => {
-// 		initiative.classList.add("show");
-// 	});
-// });
-
-// closeInit.addEventListener("click", () => {
-// 	initiative.classList.remove("show");
-// });
-
 var swiper = new Swiper(".mySwiper", {
 	slidesPerView: "2",
 	spaceBetween: 30,
